82733b8aa3480a51f6ac10e88512c4adcc1c9d8a
Goennigoegoe <79897009+Goennigoegoe@users.noreply.github.com>
2022-02-01 20:32:16 +0100

    Add files via upload

---
+++ mathapi.h
@@ -0,0 +1,20 @@
+#pragma once
+
+class math {
+public:
+	int num1;
+	int num2;
+
+	int add() {
+		return num1 + num2;
+	}
+	int subtract() {
+		return num1 - num2;
+	}
+	int times() {
+		return num1 * num2;
+	}
+	int divide() {
+		return num1 / num1;
+	}
+};
